3611cbb5314073e693cd2c70a76a5fd8ce211f38
Davi Busanello <itsme@davibusanello.net>
2018-10-03 13:58:49 -0300

    Enabled CORS to requests

--- server/src/api.js
+++ server/src/api.js
@@ -8,10 +8,22 @@ const apiVersion = '/v1';
 
 const listenPort = process.env.PORT || 3005;
 
+const allowList = [
+  'http://0.0.0.0:8080',
+  'http://localhost:8080',
+];
+const corsOptions = {
+  origin(origin, callback) {
+    const originIsAllowListed = allowList.indexOf(origin) !== -1;
+    callback(null, originIsAllowListed);
+  },
+  credentials: true,
+};
+
 api.use(morgan('combined'));
 
 api.use(express.json());
-api.use(cors());
+api.use(cors(corsOptions));
 api.use((error, request, response, next) => {
   if (error !== null) return response.status(404).json({ invalid: 'json' });
   return next();
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,6 +2,12 @@ import Router from 'express';
 
 const router = new Router();
 
+/* enable CORS preflight */
+router.options('/login');
+
+/**
+ * Route /login
+ */
 router.post('/login', (request, response) => {
   const result = {
     success: true,
