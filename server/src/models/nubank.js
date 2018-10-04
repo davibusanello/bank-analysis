5a0a23a0fecbf9f0c7aef10ef64390eb13f927e8
Davi Busanello <itsme@davibusanello.net>
2018-10-04 02:25:22 -0300

    Update the Nubank model to handle the whole flow of crawling information

--- server/src/models/nubank.js
+++ server/src/models/nubank.js
@@ -1,21 +1,27 @@
-import crawler from '../crawler/nubank/collect-information';
+import crawler from '../crawler/nubank';
 
 const NUBANK_URL = 'https://app.nubank.com.br';
 
 /**
  * Method to handle the process of collect the information from Nubank Web App
  * @param {Object} authenticationInfo {username: 'user', password: 'pass'}
+ * @returns {Object} {success, message, data}
  */
 async function getInformationFromNubankServer(authenticationInfo) {
-  const [username, password] = [authenticationInfo.username, authenticationInfo.password];
-  const result = await crawler(`${NUBANK_URL}/#/login`, username, password, {
-    headless: false,
-  })
-    .then(value => value)
-    .catch((error) => {
-      throw new Error(error.message);
-    });
-  return result;
+  try {
+    const [username, password] = [authenticationInfo.username, authenticationInfo.password];
+    const result = await crawler(NUBANK_URL, username, password)
+      .then(value => value)
+      .catch((error) => {
+        throw new Error(error.message);
+      });
+    return result;
+  } catch (error) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
 }
 
 export default getInformationFromNubankServer;
